a564bc9e6dc367bccd8b3bdd05edcd64a67ed699
MarcoMarano <x81000992@studium.unict.it>
2022-05-18 22:41:48 +0200

    Aggiunto il metodo per estrarre i dati dalle info season

--- DataIngestion/F1Data/CollectInfo.py
+++ DataIngestion/F1Data/CollectInfo.py
@@ -1,3 +1,4 @@
+from cmath import inf
 from datetime import datetime
 
 import time
@@ -14,7 +15,7 @@ import http.client
 
 
 key = "ah6gjf62tdkfm2xy89w2pg9n"
-
+CurrentSeasonId = "sr:stage:937183"
 
 
 def Send_To(host, port , data):
@@ -45,39 +46,59 @@ def CollectSeasonsInfos():
     return data
 
 
-def SaveIntoFile(data):
-    outputFile  = open("F1LiveData.json",'w')
-    outputFile.write(data)
-    outputFile.close()
 
-
-
-def CollectStageInfoFromStageId(Id):
+def CollectStageInfoFromStageId():
     conn = http.client.HTTPSConnection("api.sportradar.us")
-    conn.request("GET", "/formula1/trial/v2/en/sport_events/sr:stage:"+Id+"/summary.json?api_key=" + key)
+    conn.request("GET", "/formula1/trial/v2/en/sport_events/"+CurrentSeasonId+"/summary.json?api_key=" + key)
     res = conn.getresponse()
-    rawdata = res.read().decode('utf-8')
+    rawdata = res.read()
     return rawdata
 
-def ExtarctData(data):
-    season = json.loads(data)
-    stages = season["stages"]
-    for stage in stages:
-        description = stage["description"]
-        if description == "Formula 1 2022":
-            rawId = stage["id"]
-            campionatoID = rawId[9:]
-    stageSummary = CollectStageInfoFromStageId(campionatoID)
-    summary = json.loads(stageSummary)
 
-    team = summary['teams']
-    pilot = team['competitors']
+def CollectMoreINFOFromStageId():
+    conn = http.client.HTTPSConnection("api.sportradar.us")
 
-    
-    return  pilot
+    conn.request("GET", "/formula1/trial/v2/us/sport_events/"+"sr:stage:324773"+"/probabilities.json?api_key="+ key)
+
+    res = conn.getresponse()
+    data = res.read()
+    return data
 
 
+def ExtractData(RawDataINFO):  #RawDataINFO corrisponde a seasonFile quindi conterra tutte le info 
+                               # della stagione attuale
+    Circuiti = RawDataINFO['stage']['stages']
+    Piloti   = RawDataINFO['stage']['competitors']
+
+    return Piloti
 
 
-data = ExtarctData(CollectSeasonsInfos())
-print(data)
+##! SERVE  A RACCOGLIERE I DATI DELLA SEASON CHE MOMENTANEAMENTE PER
+##! DEBUG E COSTRUZIONE SONO SALVATI NEL FILE JSON 
+##! QUINDI LEGGERE QUEL FILE PER EVITARE DI FARE TANTE RICHIESTE
+
+# SeasonINFO = CollectStageInfoFromStageId()
+
+
+InFile = open("F1LiveData.json")
+SeasonFIle = json.loads(InFile.read())
+
+outFile = open("Info.json","w")
+data = CollectMoreINFOFromStageId()
+
+info = json.loads(data)
+outFile.write(json.dumps(info))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
